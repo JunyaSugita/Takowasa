ed4d45be3587982b5eb3f9f17cf83ec0057f8096
チャン スギ <junjunchan64@gmail.com>
2022-11-05 01:24:16 +0900

    関数被りを削除

--- Bezier.cpp
+++ Bezier.cpp
@@ -1,10 +1,5 @@
 #include "Bezier.h"
 
-//•âŠ®ŠÖ”
-const Vector3 lerp(const Vector3& start, const Vector3& end, const float t) {
-	return start * (1.0f - t) + end * t;
-};
-
 Vector3 Bezier(Vector3 start, Vector3 end, float time, Vector3 p1)
 {
 	Vector3 p3 = lerp(start, p1, time);
