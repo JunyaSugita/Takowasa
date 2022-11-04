bb5436c26185c0a2c97631dd3815cee418106049
チャン スギ <junjunchan64@gmail.com>
2022-11-05 01:22:31 +0900

    ベジエ曲線追加

---
+++ Bezier.cpp
@@ -0,0 +1,16 @@
+#include "Bezier.h"
+
+//ï‚äÆä÷êî
+const Vector3 lerp(const Vector3& start, const Vector3& end, const float t) {
+	return start * (1.0f - t) + end * t;
+};
+
+Vector3 Bezier(Vector3 start, Vector3 end, float time, Vector3 p1)
+{
+	Vector3 p3 = lerp(start, p1, time);
+	Vector3 p4 = lerp(p1, end, time);
+
+	Vector3 nowPos = lerp(p3, p4, time);
+
+	return nowPos;
+}
---
+++ Bezier.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "Vector3.h"
+
+//•âŠ®ŠÖ”
+const Vector3 lerp(const Vector3& start, const Vector3& end, const float t);
+
+Vector3 Bezier(Vector3 start, Vector3 end, float time, Vector3 p1 = { 0,0,0 });
+
+
+
+//class Bezier{};
+
