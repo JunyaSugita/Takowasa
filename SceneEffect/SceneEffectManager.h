f2d9162b287181f3abfb159a204c664ef5e53531
チャン スギ <junjunchan64@gmail.com>
2022-11-05 00:10:03 +0900

    シーン遷移演出追加、演出等のプレビュー表示キーを描画

--- SceneEffect/SceneEffectManager.h
+++ SceneEffect/SceneEffectManager.h
@@ -3,6 +3,7 @@
 #include "Sprite.h"
 #include "NomalSceneEffect.h"
 #include "Check.h"
+#include "Check2.h"
 
 
 class SceneEffectManager
@@ -14,10 +15,12 @@ public:
 
 	void NormalSceneEffectGenerate(uint32_t texture);
 	void CheckGenerate(uint32_t texture);
+	void Check2Generate(uint32_t texture);
 
 private:
 	std::list<std::unique_ptr<NomalSceneEffect>> nomal_;
-	std::list<std::unique_ptr<Check>> fade_;
+	std::list<std::unique_ptr<Check>> check_;
+	std::list<std::unique_ptr<Check2>> check2_;
 
 	uint32_t* texture_;
 };
