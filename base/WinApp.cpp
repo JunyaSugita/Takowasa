d6666c8bc9b88c83246b8c96bd220f187978fb57
LE2A_04_Yuki_iida <k021g1333@g.neec.ac.jp>
2022-11-24 10:30:58 +0900

    ウィンドウの名前変更

--- base/WinApp.cpp
+++ base/WinApp.cpp
@@ -35,7 +35,7 @@ BOOL UnadjustWindowRectEx(LPRECT prc, DWORD dwStyle, BOOL fMenu, DWORD dwExStyle
 }
 } // namespace
 
-const wchar_t WinApp::kWindowClassName[] = L"DirectXGame";
+const wchar_t WinApp::kWindowClassName[] = L"ボコパン";
 
 WinApp* WinApp::GetInstance() {
 	static WinApp instance;
--- base/WinApp.h
+++ base/WinApp.h
@@ -46,7 +46,7 @@ class WinApp {
 	/// <param name="clientHeight">ウィンドウのクライアント領域の初期高さ</param>
 	/// </summary>
 	void CreateGameWindow(
-	  const char* title = "DirectXGame", UINT windowStyle = WS_OVERLAPPEDWINDOW,
+	  const char* title = "ボコパン", UINT windowStyle = WS_OVERLAPPEDWINDOW,
 	  int32_t clientWidth = kWindowWidth, int32_t clientHeight = kWindowHeight);
 
 	/// <summary>
